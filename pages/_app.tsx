4afd38f7306120b79eece9f66a26f17f983c4cdd
Suda Ryota <r_suda0913@ezweb.ne.jp>
2024-10-26 17:20:43 +0900

    prettier追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 
 /* eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types */
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-    <>
-      <Head>
-        <title>Lifeplan</title>
-      </Head>
-      <Component
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...pageProps}
-      />
-    </>
-  );
+  <>
+    <Head>
+      <title>Lifeplan</title>
+    </Head>
+    <Component
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...pageProps}
+    />
+  </>
+);
 
 export default MyApp;
