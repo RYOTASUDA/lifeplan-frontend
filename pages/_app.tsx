6342300a3d1bcc948416b93b9a35b88f1b7124e5
Suda Ryota <r_suda0913@ezweb.ne.jp>
2024-10-27 19:56:57 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import React from 'react';
 /* eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types */
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <>
-    <Head>
+    
       <title>Lifeplan</title>
-    </Head>
+    
     <Component
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...pageProps}
